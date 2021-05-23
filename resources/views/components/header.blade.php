d694e1322159ac6fbbc7ed44b4876e1eee62638f
zerckel <antoinecarbonnel@yahoo.fr>
2021-05-23 15:36:33 +0200

    Integration header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,27 @@
 <header>
-    <div id="logo">
-        SERVO
+    <div class="container">
+        <div id="logo">
+            Servo
+        </div>
+        <label for="searchbar">
+            <input type="text" id="searchbar">
+            <span>
+                <img src="{{ asset('img/search.webp') }}" alt="searchbar">
+            </span>
+        </label>
+        <nav class="navigation">
+            <span>
+                Nos produits
+            </span>
+                <span>
+                Promos
+            </span>
+            <a href="">
+                <img src="{{ asset('img/account.webp') }}" alt="account">
+            </a>
+            <a href="">
+                <img src="{{ asset('img/basket.webp') }}" alt="basket">
+            </a>
+        </nav>
     </div>
-    <label for="searchbar">
-        <input type="text" id="searchbar">
-        <img src="" alt="searchbar">
-    </label>
-    <nav class="navigation">
-        <span>
-            Nos produits
-        </span>
-        <span>
-            Promos
-        </span>
-        <a href="">
-            <img src="{{ asset('img/account.webp') }}" alt="account">
-        </a>
-        <a href="">
-            <img src="{{ asset('img/basket.webp') }}" alt="basket">
-        </a>
-    </nav>
 </header>
